7e60967900e71d52db3a7eb560bb1549440777c4
Paulo Henrique De Queiroz Lopes <paulohq@imd.local>
2016-11-17 20:22:11 -0300

    Fazendo controle de login

---
+++ src/br/imd/zmplayer/controller/FXMLoginController.java
@@ -0,0 +1,41 @@
+package br.imd.zmplayer.controller;
+
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+
+
+public class FXMLoginController implements Initializable {
+
+    @FXML
+    private Button btnLogar;
+    public TextField userTextField;
+    public PasswordField passwordField;
+
+    @FXML
+    private void handleLoginButtonAction(ActionEvent event){
+    	if (userTextField.getText().equals("admin") && passwordField.getText().equals("admin")){
+        
+            System.out.println("logou como admin");
+        } else {
+        	System.out.println("Login invalido");
+        }
+
+        
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+}
---
+++ src/br/imd/zmplayer/model/ZMplayer.java
@@ -0,0 +1,29 @@
+package br.imd.zmplayer.model;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ZMplayer extends Application {
+
+	public void start(Stage stage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("/view/FXMLLoginScene.fxml"));
+		Scene scene = new Scene(root);
+
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	
+	
+	/**
+	 * @param args
+	 *            the command line arguments
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
