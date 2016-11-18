3ffa8628df54a385c8a41e4e5edc31378009b736
Paulo Lopes <Paulo Lopes>
2016-11-18 11:25:14 -0200

    trabalhando as transições de scenes

--- src/br/imd/zmplayer/controller/FXMLoginController.java
+++ src/br/imd/zmplayer/controller/FXMLoginController.java
@@ -1,16 +1,23 @@
 package br.imd.zmplayer.controller;
 import br.imd.zmplayer.*;
 
-
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.animation.FadeTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.util.Duration;
 
 
 
@@ -20,15 +27,35 @@ public class FXMLoginController implements Initializable {
     private Button btnLogar;
     public TextField userTextField;
     public PasswordField passwordField;
+    public Label lbLoginInfo;
+    
 
     @FXML
-    private void handleLoginButtonAction(ActionEvent event){
+    private void handleLoginButtonAction(ActionEvent event) throws IOException{
+    	Stage stage;
+    	Parent root;
     	if (userTextField.getText().equals("admin") && passwordField.getText().equals("admin")){
-        
+    		stage=(Stage) btnLogar.getScene().getWindow();
+    		root = FXMLLoader.load(getClass().getResource("../view/FXMLPlayerScene.fxml"));
+    		/*
+    		FadeTransition ft = new FadeTransition(Duration.millis(2000), root);
+    		ft.setFromValue(0.0);
+    		ft.setToValue(1.0);
+    		ft.play();
+    		*/
+    		
+    		
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
             System.out.println("logou como admin");
         } else {
+        	
+        	lbLoginInfo.setText("Usuário/Senha Inválidos");
+        	
         	System.out.println("Login invalido");
         }
+    	
 
         
     }
