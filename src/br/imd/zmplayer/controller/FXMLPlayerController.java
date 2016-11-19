0e708e64251cee2c9be17d63ded24285eb2a5498
Paulo Lopes <Paulo Lopes>
2016-11-19 19:04:57 -0200

    Tocando musicas

--- src/br/imd/zmplayer/controller/FXMLPlayerController.java
+++ src/br/imd/zmplayer/controller/FXMLPlayerController.java
@@ -3,42 +3,39 @@ package br.imd.zmplayer.controller;
 import br.imd.zmplayer.*;
 import br.imd.zmplayer.controller.utils.OperationalController;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.BufferedInputStream;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import java.io.File;
 
-import javazoom.jl.decoder.JavaLayerException;
-import javazoom.jl.player.Player;
 
-import javafx.animation.FadeTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
-import javafx.scene.media.Media;
+
 import javafx.stage.FileChooser;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-import javafx.util.Duration;
+
 
 public class FXMLPlayerController implements Initializable {
 
-	@FXML
-	public MenuItem closeButton;
-	public MenuBar menuBar;
-	public MenuItem menuUsuario;
-	public MenuItem menuOpenFile;
-	public PlayerController pc;
+	@FXML public MenuItem closeButton;
+	@FXML public MenuBar menuBar;
+	@FXML public MenuItem menuUsuario;
+	@FXML public MenuItem menuOpenFile;
+	
+	
+	private PlayerController pc;
 
 	@FXML
 	private void menuUsuarioAction(ActionEvent event) throws IOException {
@@ -57,26 +54,17 @@ public class FXMLPlayerController implements Initializable {
 		FileChooser fileChooser = new FileChooser();
 		fileChooser.setTitle("Abrir mp3");
 		fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Arquivo mp3", "*.mp3"));
-
 		File selectedFile = fileChooser.showOpenDialog(null);
 		if (selectedFile != null) {
 			try{
-				File file = new File(selectedFile.getAbsolutePath());
-				FileInputStream fis = new FileInputStream(file);
-				BufferedInputStream bis = new BufferedInputStream(fis);
-				
-				
-				try {
-					Player player = new Player(bis);
-					System.out.println("Seduzindo com "+selectedFile.getName());
-					player.play();
-				} catch (JavaLayerException e) {
-					System.out.println("Não pude abrir o arquivo");
-				}
-			} catch (IOException e){
+				pc = new PlayerController();
+				pc.tocar(selectedFile);
+			}catch (Exception e){
 				e.printStackTrace();
 			}
-
+			
+			
+			
 		}
 
 		
--- src/br/imd/zmplayer/controller/PlayerController.java
+++ src/br/imd/zmplayer/controller/PlayerController.java
@@ -1,52 +1,35 @@
 package br.imd.zmplayer.controller;
 
-import java.io.File;
-
-import javafx.stage.FileChooser;
-
-
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.Scanner;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.control.Menu;
-import javafx.scene.control.TextArea;
-import javafx.stage.FileChooser;
 
-public class PlayerController implements Initializable{
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.Player;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
-	final FileChooser fileChooser = new FileChooser();
+public class PlayerController extends FXMLPlayerController {
 
-	public void selectMusic() {
-		FileChooser fileChooser = new FileChooser();
-		fileChooser.setTitle("Abrir");
-		fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Arquivo mp3", "*.mp3"));
-
-		File selectedFile = fileChooser.showOpenDialog(null);
-		if (selectedFile != null) {
-			play(selectedFile.getAbsolutePath());
-			
-		}
-
-	}
-
-	public void play(String musica) {
-		System.out.println(musica);
-	}
-
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
+	public void tocar(File file) throws FileNotFoundException {
+		FileInputStream music = new FileInputStream(file);
+		BufferedInputStream bis = new BufferedInputStream(music);
 		
+		
+		try {
+			Player player = new Player(bis);
+			System.out.println("Seduzindo com " + file.getAbsolutePath());
+			new Thread(){
+				run(){
+					player.play();
+				}
+			}.start();
+			
+		} catch (JavaLayerException e) {
+			System.out.println("Não pude abrir o arquivo");
+		}
 	}
 
 }
