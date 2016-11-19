41da1ad48be6035f08efa3c12e3b59d80ae32632
Paulo Lopes <Paulo Lopes>
2016-11-19 21:07:33 -0200

    tocando qualquer musica

--- src/br/imd/zmplayer/controller/PlayerController.java
+++ src/br/imd/zmplayer/controller/PlayerController.java
@@ -5,9 +5,8 @@ import java.util.ResourceBundle;
 
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaPlayer.Status;
 import javafx.scene.media.MediaView;
-
-
 
 import java.io.BufferedInputStream;
 import java.io.File;
@@ -15,37 +14,58 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-public class PlayerController extends FXMLPlayerController {
+public class PlayerController {
 	
+	private static PlayerController pl = new PlayerController();
 	
-	public void tocar(File file){
-		//TODO Arrumar o caso de tocar duas musicas ao mesmo tempo
-		Media media = new Media(file.toURI().toString());
-		MediaPlayer mediaPlayer = new MediaPlayer(media);
-		mediaPlayer.setAutoPlay(true);
-		MediaView mediaView = new MediaView(mediaPlayer);
+	private PlayerController(){
+		
 	}
+	
+	public static PlayerController getInstance(){
+		return pl;
+	}
+	
+	public Media media;
+	public MediaPlayer mediaPlayer;
+	public Status status;
+	
+	public void tocar(File file) {
+		media = new Media(file.toURI().toString());
+		MediaPlayer mPlayer = new MediaPlayer(media);
+		MediaControl(mPlayer);
+	}
+	
+	public void MediaControl(final MediaPlayer mediaPlayer){
+		Status status = null;
+		if(this.mediaPlayer != null){
+			status = this.mediaPlayer.getStatus();
+		}
+		
+		if(status == Status.PLAYING){
+			this.mediaPlayer.stop();
+			this.mediaPlayer = mediaPlayer;
+		}else{
+			this.mediaPlayer = mediaPlayer;		
+		}
+		this.mediaPlayer.play();
+	}
+	
+	
 
 	/*
-	 public void tocar(File file) throws FileNotFoundException {
-	 
-		FileInputStream music = new FileInputStream(file);
-		BufferedInputStream bis = new BufferedInputStream(music);
-		
-		
-		try {
-			Player player = new Player(bis);
-			System.out.println("Seduzindo com " + file.getAbsolutePath());
-			new Thread(){
-				run(){
-					player.play();
-				}
-			}.start();
-			
-		} catch (JavaLayerException e) {
-			System.out.println("Não pude abrir o arquivo");
-		}
-	}
-	*/
+	 * public void tocar(File file) throws FileNotFoundException {
+	 * 
+	 * FileInputStream music = new FileInputStream(file); BufferedInputStream
+	 * bis = new BufferedInputStream(music);
+	 * 
+	 * 
+	 * try { Player player = new Player(bis);
+	 * System.out.println("Seduzindo com " + file.getAbsolutePath()); new
+	 * Thread(){ run(){ player.play(); } }.start();
+	 * 
+	 * } catch (JavaLayerException e) {
+	 * System.out.println("Não pude abrir o arquivo"); } }
+	 */
 
 }
